8d01e4c39973124bb7eea4ef1b8a9c20afe6cd5a
Vsavilov <vsavilov@student.42madrid.com>
2022-12-20 19:15:24 +0100

    cpp_module_02::ex00

---
+++ cpp_module_02/ex00/Fixed.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+Fixed::Fixed():_bits(0) { std::cout << "Default constructor called" << std::endl; }
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+
+Fixed::Fixed( const Fixed& copy ) {
+	std::cout << "Copy construct called" << std::endl;
+	*this = copy;
+}
+
+Fixed& Fixed::operator=( const Fixed& lhs ) {
+	std::cout << "Asignation operator called" << std::endl;
+	this->_bits = lhs.getRawBits();
+	return *this;
+}
+
+int	Fixed::getRawBits( void ) const  {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_bits;
+}
+
+int	Fixed::setRawBits( int const raw ) {
+	std::cout << "setRawBits member function called" << std::endl;
+	return raw;
+}
---
+++ cpp_module_02/ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#ifndef FIXED_HPP_
+# define FIXED_HPP_
+
+class Fixed {
+	public:
+		Fixed();
+		~Fixed();
+		Fixed( const Fixed& copy);
+		Fixed& operator=( const Fixed& lhs );
+		
+		int	getRawBits( void ) const;
+		int	setRawBits( int const raw );
+	private:
+		static const int	_num_bits = 8;
+		int			_bits;
+};
+
+#endif
